da321acec8be27b7096bfc2b2fe3b460f288c556
Johannes Raggam <johannes@raggam.co.at>
2010-01-23 17:28:28 +0100

    configure event_types in calendar_tool

---
+++ collective/folderishtypes/setuphandlers.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+#
+# GNU General Public License (GPL)
+#
+__author__ = """Johannes Raggam <johannes@raggam.co.at>"""
+__docformat__ = 'plaintext'
+
+import logging
+from Products.CMFCore.utils import getToolByName
+from collective.folderishtypes.content.folderish_event import FolderishEvent
+
+logger = logging.getLogger('collective.folderishtypes')
+
+def isNotThisProfile(context, marker_file):
+    return context.readDataFile(marker_file) is None
+
+def setup(context):
+    """ Configures Products.CMFPlone.CalendarTool
+    """
+    if isNotThisProfile(context,
+                        'collective.folderishtypes-default.txt'): return
+    site = context.getSite()
+    calendar_tool = getToolByName(site, 'portal_calendar')
+    calendar_tool.calendar_types += (FolderishEvent.portal_type,)
+    logger.info('added type %s to calendar_types' % FolderishEvent.portal_type)
