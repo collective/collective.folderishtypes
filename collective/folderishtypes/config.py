46a905a89ef1edadceecdad6a76312b646f3c3ed
Johannes Raggam <raggam-nl@adm.at>
2011-02-24 13:50:57 +0100

    explanation, why schema cleanup is needed

--- collective/folderishtypes/config.py
+++ collective/folderishtypes/config.py
@@ -7,6 +7,10 @@ ADD_PERMISSIONS = {
 }
 
 def schema_cleanup(schema):
+    """ Cleanup a schema to prepare it to be merged with another one.
+        ATCT Schema definitions are not simple lists or dicts, so there is
+        some manual interaction neccessary.
+    """
     for key in ['id', 'title', 'description', 'subject', 'relatedItems',
               'location', 'language', 'effectiveDate', 'expirationDate',
               'creation_date', 'modification_date', 'creators', 'contributors',
