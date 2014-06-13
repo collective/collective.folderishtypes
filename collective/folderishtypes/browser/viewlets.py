e38ec0ecd1d783bbb624159ce059fdd5c242225b
Peter Holzer <hpeter@agitator.com>
2014-06-13 18:24:14 +0200

    added condition against folder_listing

---
+++ collective/folderishtypes/browser/viewlets.py
@@ -0,0 +1,10 @@
+from plone.app.layout.viewlets.common import ViewletBase
+from Products.CMFPlone.interfaces import IPloneSiteRoot
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+
+class ListingViewlet(ViewletBase):
+    index = ViewPageTemplateFile('listing_viewlet.pt')
+
+    def available(self):
+		return 'folder_contents' not in self.view.__name__
