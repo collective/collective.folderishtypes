2e596bc761253727f2486286beb21e1921a996f0
Johannes Raggam <raggam-nl@adm.at>
2014-10-01 15:05:55 +0200

    beautify

--- setup.py
+++ setup.py
@@ -2,36 +2,37 @@ from setuptools import setup
 from setuptools import find_packages
 
 version = '2.0dev'
+title = 'collective.folderishtypes'
+description = "Folderish News Item, Document and Event as replacement for default types."  # noqa
+long_desc = open("README.rst").read() + "\n" + open("CHANGES.rst").read()
 
-setup(name='collective.folderishtypes',
-      version=version,
-      description="Provides folderish types as a replacement for some ATContentTypes",
-      long_description=open("README.rst").read() + "\n" +
-                       open("CHANGES.rst").read(),
-      # Get more strings from http://pypi.python.org/pypi?:action=list_classifiers
-      classifiers=[
+setup(
+    name=title,
+    version=version,
+    description=description,
+    long_description=long_desc,
+    classifiers=[
         "Framework :: Plone",
         "Programming Language :: Python",
-        ],
-      keywords='ATContentTypes Archetypes Plone Folderish',
-      author='Johannes Raggam',
-      author_email='johannes@raggam.co.at',
-      url='http://github.com/collective/collective.folderishtypes',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['collective'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          # -*- Extra requirements: -*-
-      ],
-      extras_require={
-          'dexterity': ['plone.app.contenttypes', ],
-          'migration': ['Products.contentmigration', ],
-      },
-      entry_points="""
+    ],
+    keywords='content types plone folderish',
+    author='Johannes Raggam',
+    author_email='johannes@raggam.co.at',
+    url='http://github.com/collective/collective.folderishtypes',
+    license='GPL',
+    packages=find_packages(),
+    namespace_packages=['collective'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+    ],
+    extras_require={
+        'dexterity': ['plone.app.contenttypes', ],
+        'migration': ['Products.contentmigration', ],
+    },
+    entry_points="""
       [z3c.autoinclude.plugin]
       target = plone
       """,
-      )
+)
