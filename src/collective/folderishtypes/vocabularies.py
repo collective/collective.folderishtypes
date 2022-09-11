c5fc8bd2f1697f3b50afbdb75678a8726e230eeb
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2022-09-11 18:26:39 +0000

    Fix Plone 6 where getAllowedSizes() moved to plone.namedfile.

--- src/collective/folderishtypes/vocabularies.py
+++ src/collective/folderishtypes/vocabularies.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 try:
-    from Products.CMFPlone.utils import getAllowedSizes
+    from plone.namedfile.utils import getAllowedSizes
 except ImportError:
-    from plone.app.imaging.utils import getAllowedSizes
+    try:
+        from plone.app.imaging.utils import getAllowedSizes
+    except ImportError:
+        from Products.CMFPlone.utils import getAllowedSizes
 from zope.interface import provider
 from zope.schema.interfaces import IVocabularyFactory
 from zope.schema.vocabulary import SimpleVocabulary
@@ -12,7 +15,8 @@ from zope.schema.vocabulary import SimpleVocabulary
 def ImageScaleVocabulary(context):
     allowed_sizes = getAllowedSizes()
     items = [
-        (u'%s(%s, %s)' % (key, value[0], value[1]), key)
-        for key, value in allowed_sizes.items() if allowed_sizes
+        ("%s(%s, %s)" % (key, value[0], value[1]), key)
+        for key, value in allowed_sizes.items()
+        if allowed_sizes
     ]
     return SimpleVocabulary.fromItems(items)
