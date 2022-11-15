04d12bd1c67ac36684318cbbaa073c5e46d564d7
Johannes Raggam <thetetet@gmail.com>
2022-11-15 01:14:45 +0100

    Code formating.

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
-from setuptools import setup
-from setuptools import find_packages
+from setuptools import find_packages, setup
 
-version = '3.0.1.dev0'
-title = 'collective.folderishtypes'
-description = "Folderish News Item, Document and Event as replacement for default types."  # noqa
+version = "3.0.1.dev0"
+title = "collective.folderishtypes"
+description = (
+    "Folderish News Item, Document and Event as replacement for default types."  # noqa
+)
 long_desc = open("README.rst").read() + "\n" + open("CHANGES.rst").read()
 
 setup(
@@ -21,26 +22,30 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
     ],
-    keywords='content types plone folderish',
-    author='Johannes Raggam',
-    author_email='johannes@raggam.co.at',
-    url='http://github.com/collective/collective.folderishtypes',
-    license='GPL',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    namespace_packages=['collective'],
+    keywords="content types plone folderish",
+    author="Johannes Raggam",
+    author_email="johannes@raggam.co.at",
+    url="http://github.com/collective/collective.folderishtypes",
+    license="GPL",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    namespace_packages=["collective"],
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'setuptools',
-        'zope.interface',
-        'zope.schema',
-        'zope.i18nmessageid',
+        "setuptools",
+        "zope.interface",
+        "zope.schema",
+        "zope.i18nmessageid",
     ],
     extras_require={
-        'archetypes': ['Products.ATContentTypes', 'plone.app.imaging'],
-        'dexterity': ['plone.app.contenttypes', ],
-        'migration': ['Products.contentmigration', ],
+        "archetypes": ["Products.ATContentTypes", "plone.app.imaging"],
+        "dexterity": [
+            "plone.app.contenttypes",
+        ],
+        "migration": [
+            "Products.contentmigration",
+        ],
     },
     entry_points="""
       [z3c.autoinclude.plugin]
